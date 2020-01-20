cf16d21f1849b776c079c312210956727d16ba2e
Kishlaya Jaiswal <kishlaya.j@gmail.com>
2020-01-20 10:57:28 +0530

    Modularized and added more functions

--- main.py
+++ main.py
@@ -3,31 +3,24 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from numpy import linalg as LA
 
-# Number of vertices
-n = int(input())
-
 # Generate a random adjacency matrix
-adj = np.zeros((n,n))
+def random_graph(n):
+    adj = np.zeros((n,n))
 
-for i in range(0,n):
- for j in range(i+1,n):
-  r = np.random.uniform(0,1)
-  adj[i][j] = round(r)
-  adj[j][i] = adj[i][j]
+    for i in range(0,n):
+        for j in range(i+1,n):
+            r = np.random.uniform(0,1)
+            adj[i][j] = round(r)
+            adj[j][i] = adj[i][j]
 
-print("Adjacency matrix : ")
-print(adj)
-print()
+    return adj
 
 # Find eigenvalues/vectors for the adj matrix
-w,v = LA.eig(adj)
-print("Eigenvalue : Eigenvector")
-for i in range(0,len(w)):
- print(w[i], ":", v[:,i])
- print()
+def eigen(adjacency_matrix):
+    return LA.eig(adjacency_matrix)
 
 # Draw graph
-def show_graph_with_labels(adjacency_matrix):
+def show_graph(adjacency_matrix):
     rows, cols = np.where(adjacency_matrix == 1)
     edges = zip(rows.tolist(), cols.tolist())
     gr = nx.Graph()
@@ -35,4 +28,12 @@ def show_graph_with_labels(adjacency_matrix):
     nx.draw(gr, node_size=250, with_labels=True)
     plt.show()
 
-show_graph_with_labels(adj)
+def find_degree(adjacency_matrix, i):
+    return sum(adjacency_matrix[i])
+
+def is_connected(adjacency_matrix):
+    mu, phi = eigen(adjacency_matrix)
+    mu.sort()
+    if mu[-2] == mu[-1]:
+        return False
+    return True
